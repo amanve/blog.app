5b8cfc539b2a25406467645480313f09473fa528
Aman <aman.kash94@gmail.com>
2022-04-01 12:52:19 +0530

    pagination done

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from wtforms import StringField, TextAreaField, EmailField, TelField
 from wtforms.validators import InputRequired
 from datetime import datetime
 from sqlalchemy import desc
+from sqlalchemy.ext.hybrid import hybrid_property
 import pandas as pd
 
 # Create App/Configurations
@@ -16,6 +17,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['POSTS_PER_PAGE'] = 10
+app.config['COMMENTS_PER_PAGE'] = 5
 
 # Flask Config
 app.config['SECRET_KEY'] = 'mysecret!'
@@ -136,6 +138,55 @@ def index():
                            prev_url=prev_url)
 
 
+@app.route('/post/<post_id>', methods=['GET', 'POST'])
+def post(post_id):
+    form = NewComment()
+    post = Post.query.get(int(post_id))
+    comments_page = request.args.get('comments_page', 1, type=int)
+
+    if form.validate_on_submit():
+        comment = Comments(user_id=current_user.id,
+                           comment=form.comment.data,
+                           dateCreated=datetime.now())
+        post.comments.append(comment)
+        db.session.commit()
+
+        return redirect(url_for('post', post_id=post.id))
+    comments = Comments.query.filter_by(post_id=post_id).order_by(
+        desc(Comments.dateCreated)).paginate(comments_page,
+                                             app.config['COMMENTS_PER_PAGE'],
+                                             True)
+    # comments = Comments.query.filter_by(post_id=post_id).all()
+
+    next_url = url_for('post', comments_page=comments.next_num, post_id=post.id) \
+    if comments.has_next else None
+    prev_url = url_for('post', comments_page=comments.prev_num, post_id=post.id) \
+    if comments.has_prev else None
+
+    return render_template('post.html',
+                           post=post,
+                           form=form,
+                           comments=comments,
+                           next_url=next_url,
+                           prev_url=prev_url)
+
+
+@app.route('/add_post', methods=['GET', 'POST'])
+def add_post():
+    form = NewPost()
+    if form.validate_on_submit():
+        new_post = Post(heading=form.heading.data,
+                        subHeading=form.subHeading.data,
+                        body=form.body.data,
+                        dateCreated=datetime.now())
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+    return render_template('add_post.html', form=form)
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -164,43 +215,6 @@ def contact():
         return redirect(url_for('contact'))
 
     return render_template('contact.html', form=form)
-
-
-# @app.route('/post')
-@app.route('/post/<post_id>', methods=['GET', 'POST'])
-def post(post_id):
-    form = NewComment()
-    post = Post.query.get(int(post_id))
-
-    if form.validate_on_submit():
-        comment = Comments(user_id=current_user.id,
-                           comment=form.comment.data,
-                           dateCreated=datetime.now())
-        post.comments.append(comment)
-        db.session.commit()
-
-        return redirect(url_for('post', post_id=post.id))
-
-    comments = Comments.query.filter_by(post_id=post_id).order_by(
-        desc(Comments.dateCreated)).all()
-
-    return render_template('post.html', post=post, form=form, comments=comments)
-
-
-@app.route('/add_post', methods=['GET', 'POST'])
-def add_post():
-    form = NewPost()
-    if form.validate_on_submit():
-        new_post = Post(heading=form.heading.data,
-                        subHeading=form.subHeading.data,
-                        body=form.body.data,
-                        dateCreated=datetime.now())
-        db.session.add(new_post)
-        db.session.commit()
-
-        return redirect(url_for('index'))
-
-    return render_template('add_post.html', form=form)
 
 
 #Execute App
