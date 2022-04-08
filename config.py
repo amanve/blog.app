10c856be07a95409492cc966a66a8e08648defc9
Aman <aman.kash94@gmail.com>
2022-04-08 18:09:06 +0530

    fix structure related issues

--- config.py
+++ config.py
@@ -7,8 +7,10 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'myseceretkey!'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     POSTS_PER_PAGE = 10
     COMMENTS_PER_PAGE = 5
+
     SECURITY_REGISTERABLE = True
     SECURITY_PASSWORD_SALT = 'somesaltforblog!'
     SECURITY_SEND_REGISTER_EMAIL = False
