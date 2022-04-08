52e13cdd47e4e3004df116b2d04dd63b33d36676
Aman <aman.kash94@gmail.com>
2022-04-08 12:42:38 +0530

    updated config file

--- config.py
+++ config.py
@@ -5,9 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'myseceretkey!'
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL'
-    ) or 'mysql+mysqlconnector://root:password@localhost/amve_blog'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 10
     COMMENTS_PER_PAGE = 5
@@ -15,3 +13,34 @@ class Config(object):
     SECURITY_PASSWORD_SALT = 'somesaltforblog!'
     SECURITY_SEND_REGISTER_EMAIL = False
     SECURITY_USER_IDENTITY_ATTRIBUTES = ('username', 'email')
+
+
+@staticmethod
+def init_app(app):
+    pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DEV_DATABASE_URL'
+    ) or 'mysql+mysqlconnector://root:password@localhost/amve_blog'
+
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+'sqlite:///blog_test.db'
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
